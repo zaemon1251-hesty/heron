170e528e8e1dac30a947d19ef8bebf056de9dd55
zaemon1251-hesty <hisakawa1251@gmail.com>
2023-09-30 16:25:59 +0900

    good case for decoding so far

--- scripts/run_soccernet_demo.py
+++ scripts/run_soccernet_demo.py
@@ -1,37 +1,47 @@
 import sys
 
-sys.path.append(".")
-
-from heron.models.video_blip import VideoBlipForConditionalGeneration
-from heron.models.utils import (
-    load_pretrained_weight,
-)
-
-import requests
 from PIL import Image
 
 import torch
 from transformers import AutoProcessor
+import numpy as np
+import cv2
+from loguru import logger
+import yaml
 
-device_id = 0
+try:
+    from heron.models.utils import (
+        load_pretrained_weight,
+        load_model,
+    )
+except ModuleNotFoundError:
+    sys.path.append(".")
+    from heron.models.utils import (
+        load_pretrained_weight,
+        load_model,
+    )
 
-model_config = {
-    "pretrained_path": "/raid/moriy/model/heron/video_blip_soccernet/video_blip_soccernet/exp001/video_blip_soccernet/exp001_final",
-    "language_model_name": "stabilityai/japanese-stablelm-base-alpha-7b",
-    "num_frames": 1,
-    "max_length": 256,
-    "temperature": 0.0,
-}
+
+logger.add("log/demo.log", rotation="500 MB")
+
+device_id = 1
+
+config_file_path = "projects/video_blip_soccernet/exp001.yml"
+with open(config_file_path, "r") as i_:
+    config = yaml.safe_load(i_)
+    model_config = config["model_config"]
+    training_config = config["training_config"]
+
+model_config[
+    "pretrained_path"
+] = "/raid/moriy/model/heron/video_blip_soccernet/video_blip_soccernet/exp001/video_blip_soccernet/exp001_final"
+model_config["temperature"] = 0.0
 
 img_path = "data/SoccerNet/raw_images/         6/frame_0001.jpg"
 gold_caption = "[PLAYER] ([TEAM]) volleys the ball from just outside the box, but his effort goes narrowly over the bar."  # https://www.soccer-net.org/
 
 # prepare a pretrained model
-model = VideoBlipForConditionalGeneration.create(
-    model_config["language_model_name"],
-    model_config["num_frames"],
-    torch_dtype=torch.float16,
-)
+model = load_model(model_config)
 load_pretrained_weight(model, model_config["pretrained_path"])
 model.eval()
 model.to(f"cuda:{device_id}")
@@ -40,13 +50,16 @@ model.to(f"cuda:{device_id}")
 processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
 
 # prepare inputs
-image = Image.open(img_path)
-
+img = Image.open(img_path).convert("RGB")
+img = np.array(img)
+if img.shape[2] != 3:
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+imgs = [img]
 text = ""
 
 # do preprocessing
 inputs = processor(
-    images=image,
+    images=imgs,
     text=text,
     return_tensors="pt",
     max_length=model_config["max_length"],
@@ -54,6 +67,7 @@ inputs = processor(
     truncation=True,
 )
 inputs = {k: v.to(f"cuda:{device_id}") for k, v in inputs.items()}
+inputs["pixel_values"] = inputs["pixel_values"].half()
 
 # set eos token
 eos_token_id_list = [
@@ -72,5 +86,5 @@ with torch.no_grad():
     )
 
 # print result
-print(processor.tokenizer.batch_decode(out)[0])
-print(gold_caption)
+logger.info(processor.tokenizer.batch_decode(out)[0])
+logger.info(gold_caption)
